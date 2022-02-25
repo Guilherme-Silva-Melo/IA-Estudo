7061cf88113c4d8b0df8704a52c3632db3df04dd
Gui Melo <guilherme.melo@solucaut.com.br>
2022-02-24 23:43:06 -0300

    att_estudo_mnist

--- teste_ia/estudo_mnist.py
+++ teste_ia/estudo_mnist.py
@@ -1,4 +1,4 @@
-import keras 
+import tensorflow as tf
 from keras.datasets import mnist
 from keras.models import Sequential 
 from keras.layers import Dense 
@@ -6,11 +6,11 @@ from numpy import reshape
 
 (x,y),(_,_) = mnist.load_data()
 
-x = reshape(x,)
+x = x.reshape(60000,784)
 
 x = x/255.0
 
-y = keras.utils.to_categorical(y,10)
+y = tf.keras.utils.to_categorical(y,10)
 
 model = Sequential()
 
@@ -18,7 +18,7 @@ model.add(Dense(64,input_shape=(784,),activation='relu'))
 model.add(Dense(64,activation='relu'))
 model.add(Dense(10,activation='softmax'))
 
-model.compile(optimizer='adam',loss='categorical_crossentropy',metrics='[acc]')
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics='acc')
 model.fit(x,y, batch_size=128, epochs = 10)
 
 print(model.predict(x))
